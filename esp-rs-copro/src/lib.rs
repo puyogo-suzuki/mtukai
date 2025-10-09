d08b2315cd3560b51461ada0b0199899acdd2d35
Go Suzuki <puyogo.suzuki@gmail.com>
2025-10-09 11:30:11 +0900

    [REMOVE] Unnecessary test created by cargo.

--- esp-rs-copro/src/lib.rs
+++ esp-rs-copro/src/lib.rs
@@ -4,14 +4,3 @@ pub trait MovableObject {
     fn move_to_main(&self) -> *mut u8;
     unsafe fn move_to_lp(&self) -> *mut u8;
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
