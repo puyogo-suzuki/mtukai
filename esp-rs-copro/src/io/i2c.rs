88e829c22761c51ff3909eb082ed4944e8bfa407
Go Suzuki <puyogo.suzuki@gmail.com>
2025-11-12 10:39:33 +0900

    [ADD] esp_rs_copro::io::i2c

---
+++ esp-rs-copro/src/io/i2c.rs
@@ -0,0 +1,81 @@
+use crate::movableobject::MovableObject;
+
+pub struct LPI2C {
+    #[cfg(feature = "has-lp-core")]
+    i2c : esp_hal::i2c::lp_i2c::LpI2c,
+    #[cfg(feature = "is-lp-core")]
+    i2c : esp_lp_hal::i2c::LpI2c,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum LPI2CError {
+    ExceedingFifo,
+    AckCheckFailed,
+    TimeOut,
+    ArbitrationLost,
+    ExecIncomplete,
+    CommandNrExceeded,
+    InvalidResponse,
+}
+
+impl LPI2CError {
+    #[cfg(feature = "is-lp-core")]
+    fn convert_from_result<T>(val : Result<T, esp_lp_hal::i2c::Error>) -> Result<T, LPI2CError> {
+        match val {
+            Ok(v) => Ok(v),
+            Err(e) => match e {
+                esp_lp_hal::i2c::Error::ExceedingFifo => Err(LPI2CError::ExceedingFifo),
+                esp_lp_hal::i2c::Error::AckCheckFailed => Err(LPI2CError::AckCheckFailed),
+                esp_lp_hal::i2c::Error::TimeOut => Err(LPI2CError::TimeOut),
+                esp_lp_hal::i2c::Error::ArbitrationLost => Err(LPI2CError::ArbitrationLost),
+                esp_lp_hal::i2c::Error::ExecIncomplete => Err(LPI2CError::ExecIncomplete),
+                esp_lp_hal::i2c::Error::CommandNrExceeded => Err(LPI2CError::CommandNrExceeded),
+                esp_lp_hal::i2c::Error::InvalidResponse => Err(LPI2CError::InvalidResponse),
+            }
+        }
+    }
+    #[cfg(feature = "has-lp-core")]
+    fn convert_from_result<T>(val : Result<T, esp_hal::i2c::lp_i2c::Error>) -> Result<T, LPI2CError> {
+        match val {
+            Ok(v) => Ok(v),
+            Err(e) => match e {
+                esp_hal::i2c::lp_i2c::Error::ExceedingFifo => Err(LPI2CError::ExceedingFifo),
+                esp_hal::i2c::lp_i2c::Error::AckCheckFailed => Err(LPI2CError::AckCheckFailed),
+                esp_hal::i2c::lp_i2c::Error::TimeOut => Err(LPI2CError::TimeOut),
+                esp_hal::i2c::lp_i2c::Error::ArbitrationLost => Err(LPI2CError::ArbitrationLost),
+                esp_hal::i2c::lp_i2c::Error::ExecIncomplete => Err(LPI2CError::ExecIncomplete),
+                esp_hal::i2c::lp_i2c::Error::CommandNrExceeded => Err(LPI2CError::CommandNrExceeded),
+                esp_hal::i2c::lp_i2c::Error::InvalidResponse => Err(LPI2CError::InvalidResponse),
+            }
+        }
+    }
+}
+
+impl LPI2C {
+    #[cfg(feature = "is-lp-core")]
+    pub fn write(&mut self, address : u8, bytes : &[u8]) -> Result<(), LPI2CError> {
+        LPI2CError::convert_from_result(self.i2c.write(address, bytes))
+    }
+    #[cfg(feature = "is-lp-core")]
+    pub fn read(&mut self, address : u8, buffer : &mut [u8]) -> Result<(), LPI2CError> {
+        LPI2CError::convert_from_result(self.i2c.read(address, buffer))
+    }
+    #[cfg(feature = "is-lp-core")]
+    pub fn write_read(
+        &mut self,
+        address: u8,
+        bytes: &[u8],
+        buffer: &mut [u8],
+    ) -> Result<(), LPI2CError> {
+        LPI2CError::convert_from_result(self.i2c.write_read(address, bytes, buffer))
+    }
+}
+
+impl MovableObject for LPI2C {
+    unsafe fn move_to_main(&self, _ : *mut u8) {
+        // DO NOTHING! because they are zero-sized types.
+    }
+    unsafe fn move_to_lp(&self, _ : *mut u8) {
+        // DO NOTHING! because they are zero-sized types.
+    }
+}
--- esp-rs-copro/src/lib.rs
+++ esp-rs-copro/src/lib.rs
@@ -4,6 +4,7 @@ extern crate alloc;
 pub mod lpalloc;
 pub mod lpbox;
 pub mod movableobject;
+pub mod io;
 #[cfg(any(feature = "has-lp-core", test))]
 mod addresstranslation;
 #[cfg(feature = "has-lp-core")]
