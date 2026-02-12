474096d7cb2bf6acbeb5ec8fa7918b7e1b61fcea
Go Suzuki <puyogo.suzuki@gmail.com>
2026-02-12 15:30:52 +0900

    [ADD] LPBox Move Test

--- esp-rs-copro/tests/lpbox.rs
+++ esp-rs-copro/tests/lpbox.rs
@@ -14,3 +14,21 @@ fn test_lpbox_alloc() {
     let mut lpbox = LPBox::new(original);
     let lp_ptr = lpbox.get_moved_to_lp();
 }
+
+#[test]
+fn test_lpbox_transfer() {
+    lpalloc::lp_allocator_init();
+    let original = TestStruct { value1: 10, value2: 20 };
+    let mut lpbox = LPBox::new(original);
+    let mut lp_ptr = lpbox.get_moved_to_lp();
+    lp_ptr.value1 = 30;
+    lp_ptr.value2 = 40;
+    assert_eq!(lpbox.value1, 10);
+    assert_eq!(lpbox.value2, 20);
+    let mut moved = lp_ptr.get_moved_to_main();
+    if lpbox.as_ptr() == moved.as_ptr() {
+        let dont_drop = core::mem::ManuallyDrop::new(lpbox);
+    }
+    assert_eq!(moved.value1, 30);
+    assert_eq!(moved.value2, 40);
+}
