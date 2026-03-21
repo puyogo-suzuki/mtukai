d6f2af90463ab4440df4736d8aaec4f151749f1c
Go Suzuki <puyogo.suzuki@gmail.com>
2026-03-21 13:39:55 +0900

    [FIX] Fix lp_core_halt. It overwrites the wakeup source.

--- esp-rs-copro/src/prelude.rs
+++ esp-rs-copro/src/prelude.rs
@@ -3,6 +3,6 @@ pub fn lp_core_halt() -> ! {
     use esp_lp_hal::pac;
 
     unsafe { &*pac::PMU::PTR }
-        .lp_cpu_pwr1().write(|x| { x.lp_cpu_sleep_req().set_bit() });
+        .lp_cpu_pwr1().modify(|_, w| { w.lp_cpu_sleep_req().set_bit() });
     loop{}
 }
